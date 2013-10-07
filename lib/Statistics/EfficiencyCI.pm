ed7d77bc21affa8d05449063101060d33052afb4
Steffen Müller <smueller@cpan.org>
2013-10-07 15:00:37 +0000

    Import of SMUELLER/Statistics-EfficiencyCI-0.05 from CPAN.
    gitpan-cpan-distribution: Statistics-EfficiencyCI
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         SMUELLER/Statistics-EfficiencyCI-0.05.tar.gz
    gitpan-cpan-author:       SMUELLER
    gitpan-cpan-maturity:     released

--- lib/Statistics/EfficiencyCI.pm
+++ lib/Statistics/EfficiencyCI.pm
@@ -4,13 +4,16 @@ use 5.008001;
 use strict;
 use warnings;
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 require Exporter;
 our @ISA = qw(Exporter);
 our @EXPORT_OK = qw(
   efficiency_ci
   log_gamma
+  beta_ab
+  ibetai
+  beta_cf
 );
 our @EXPORT_TAGS = ('all' => \@EXPORT_OK);
 
@@ -72,7 +75,7 @@ and its implementation is Marc Paterno.
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2011 by Steffen Mueller
+Copyright (C) 2011-2013 by Steffen Mueller
 
 Most of the code is taken from the ROOT library (licensed under LGPL) and
 by proxy, the CEPHES library (licensed with a modified BSD license). The full list
